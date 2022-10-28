bf0750bb4463d417320406839c7a938f0f267fa5
asteriskman <asteriskman7@users.noreply.github.com>
2022-10-28 14:05:06 -0400

    try fixing font size

--- sketch28.js
+++ sketch28.js
@@ -141,10 +141,11 @@ window.sketchClass = class extends Sketch {
       const dl = this.lmap(Math.random(), 0, 1, -5, 5);
       ctx.fillStyle = `hsl(0, 98%, ${43 + dl}%)`;
       ctx.fillRect(sx0, sy0, sw, sh);
-      ctx.font = '8px Arial';
+      ctx.font = '0.45em Arial';
       ctx.textAlign = 'center';
       ctx.textBaseline = 'middle';
       ctx.fillStyle = `hsl(0, 98%, ${90 + dl}%)`;
+      //ctx.letterSpacing = '0px';
       ctx.fillText('EMERGENCY BROADCAST SYSTEM', sx0 + sw / 2, sy0 + 10);
       const lines = [
        'THIS IS NOT A TEST',
